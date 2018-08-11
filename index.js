891f9f7b44007316a90717a4cb0a2a09c1ad169d
Mitch Lipayon <mitch.lipayon@versacomputer.com>
2018-08-11 09:00:34 -0400

    separate environment for development and production

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ import http from 'http';
 import https from 'https';
 import fs from 'fs';
 
-const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_FILE, 'utf8');
-const certificate = fs.readFileSync(process.env.CERTIFICATE_FILE, 'utf8');
-const credentials = {key: privateKey, cert: certificate};
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.TERRITORY_PORT || 4000;
+const PORT_SSL = process.env.TERRITORY_PORT_TLS || 4443;
+
+const app = express();
 
 export const conn = mysql.createConnection({
   ssl: { rejectUnauthorized: false }, // TODO: add SSL certificate file here (see https://github.com/mysqljs/mysql#ssl-options)
@@ -45,26 +47,28 @@ conn.connect((err) => {
   }
 });
 
-const PORT = process.env.TERRITORY_PORT || 4000;
-const PORT_SSL = process.env.TERRITORY_PORT_TLS || 4443;
-const app = express();
-
 app.use(cors());
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, cacheControl: true }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+if (NODE_ENV === 'production') {
+  const PRIVATE_KEY_FILE = process.env.PRIVATE_KEY_FILE || '';
+  const CERT_FILE = process.env.CERTIFICATE_FILE || '';
+  const PRIVATE_KEY = fs.readFileSync(PRIVATE_KEY_FILE, 'utf8');
+  const CERT = fs.readFileSync(CERT_FILE, 'utf8');
+  const CREDENTIALS = {key: PRIVATE_KEY, cert: CERT};
 
-/*
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
-*/
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(CREDENTIALS, app);
 
-httpServer.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
-httpsServer.listen(PORT_SSL, () => {
-  console.log(`Listening on port ${PORT_SSL}`);
-});
+  httpServer.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+  httpsServer.listen(PORT_SSL, () => {
+    console.log(`Listening on port ${PORT_SSL}`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
